3857dab19bbe9a3406d0573cf113f9dbb91b27c0
Buthmika <buthmikamenushan@gmail.com>
2024-12-17 20:19:03 +0530

    update

--- script.js
+++ script.js
@@ -2,24 +2,23 @@ let speech = new SpeechSynthesisUtterance();
 let voices = [];
 let voiceSelect = document.querySelector("#select");
 
-// Populate voices when available
 window.speechSynthesis.onvoiceschanged = () => {
       voices = window.speechSynthesis.getVoices();
-      speech.voice = voices[0]; // Default to the first available voice
+      speech.voice = voices[0]; 
 
-      voiceSelect.innerHTML = ""; // Clear existing options
+      voiceSelect.innerHTML = ""; 
       voices.forEach((voice, i) => {
-            let option = new Option(voice.name, i); // Create option element
+            let option = new Option(voice.name, i); 
             voiceSelect.add(option);
       });
 };
 
-// Set the voice based on user selection
+
 voiceSelect.addEventListener("change", () => {
       speech.voice = voices[voiceSelect.value];
 });
 
-// Speak text when button is clicked
+
 document.querySelector("#button").addEventListener("click", () => {
       speech.text = document.querySelector("#text").value;
       window.speechSynthesis.speak(speech);
